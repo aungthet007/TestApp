856269d827a08215b9c31efd088acebac7ab8559
Thet Zaw Oo <cpthetzawoo52@gmail.com>
2021-06-14 13:34:59 +0630

    testing web application

---
+++ wapApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wapApp.Startup))]
+namespace wapApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
